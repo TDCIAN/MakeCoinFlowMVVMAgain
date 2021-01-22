31ff559dd13d7ecc7a18f77fdd8bee9808930cd9
Jeongmin <tdcian71@gmail.com>
2021-01-22 22:03:30 +0900

    Article 구조체 생성

--- CoinFlowMVVMAgain/API_TEST.playground/Contents.swift
+++ CoinFlowMVVMAgain/API_TEST.playground/Contents.swift
@@ -2,9 +2,27 @@ import UIKit
 
 let urlSession = URLSession.shared
 
+//{
+//    "title": "True Life: I’m a Bitcoin",
+//    "link": "https://www.coindesk.com/true-life-im-a-bitcoin",
+//    "date": "Mon, 01 Apr 2019 16:15:16 +0000",
+//    "timestamp": 1554135316,
+//    "description": "What's it like to be bitcoin? It's not all it's hashed out to be.",
+//    "imageUrl": "http://coinbelly.com/api/images/3rdparty/coindesk.com_0.jpg"
+//},
+
+struct Article: Codable {
+    let title: String
+    let link: String
+    let date: String
+    let timestamp: Int
+    let description: String
+    let imageUrl: String
+}
+
 let newsURL = URL(string: "http://coinbelly.com/api/get_rss")!
 
-let dataTask = urlSession.dataTask(with: newsURL) { (data, response, error) in
+let taskWithNewsURL = urlSession.dataTask(with: newsURL) { (data, response, error) in
     let successRange = 200..<300
     guard error == nil,
           let statusCode = (response as? HTTPURLResponse)?.statusCode,
@@ -15,4 +33,4 @@ let dataTask = urlSession.dataTask(with: newsURL) { (data, response, error) in
     let string = String(data: responseData, encoding: .utf8)
     print(string)
 }
-dataTask.resume()
+taskWithNewsURL.resume()
