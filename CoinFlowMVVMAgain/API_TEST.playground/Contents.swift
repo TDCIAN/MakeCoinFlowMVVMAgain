c95957c1e8a4a7a660c8eae46620fba163a29570
Jeongmin <tdcian71@gmail.com>
2021-01-22 22:00:29 +0900

    responseData 처리

--- CoinFlowMVVMAgain/API_TEST.playground/Contents.swift
+++ CoinFlowMVVMAgain/API_TEST.playground/Contents.swift
@@ -5,6 +5,14 @@ let urlSession = URLSession.shared
 let newsURL = URL(string: "http://coinbelly.com/api/get_rss")!
 
 let dataTask = urlSession.dataTask(with: newsURL) { (data, response, error) in
-        
+    let successRange = 200..<300
+    guard error == nil,
+          let statusCode = (response as? HTTPURLResponse)?.statusCode,
+          successRange.contains(statusCode) else {
+        return
+    }
+    guard let responseData = data else { return }
+    let string = String(data: responseData, encoding: .utf8)
+    print(string)
 }
-
+dataTask.resume()
