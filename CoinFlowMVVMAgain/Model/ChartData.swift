fb0b3050a25b8ca3defec6f8ee23802dc0320a5b
Jeongmin <tdcian71@gmail.com>
2021-01-26 23:10:22 +0900

    Model 그룹 내 ChartData 파일 생성

--- CoinFlowMVVMAgain/Model/ChartData.swift
+++ CoinFlowMVVMAgain/Model/ChartData.swift
@@ -6,3 +6,21 @@
 //
 
 import Foundation
+
+struct ChartDataResponse: Codable {
+    let chartDatas: [ChartData]
+    
+    enum CodingKeys: String, CodingKey {
+        case chartDatas = "Data"
+    }
+}
+
+struct ChartData: Codable {
+    let time: TimeInterval
+    let closePrice: Double
+    
+    enum CodingKeys: String, CodingKey {
+        case time
+        case closePrice = "close"
+    }
+}
