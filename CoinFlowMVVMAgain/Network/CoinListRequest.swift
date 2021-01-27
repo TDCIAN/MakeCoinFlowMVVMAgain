b8af5b1ef68be4b5518de556eea349ddabe863ec
Jeongmin <tdcian71@gmail.com>
2021-01-27 11:21:34 +0900

    URL의 기본 뼈대가 되는 EndPoint, 내부 네트워킹을 더 용이하게 해주는 Request 생성

---
+++ CoinFlowMVVMAgain/Network/CoinListRequest.swift
@@ -0,0 +1,20 @@
+//
+//  CoinListRequest.swift
+//  CoinFlowMVVMAgain
+//
+//  Created by APPLE on 2021/01/27.
+//
+
+import Foundation
+
+struct CoinListRequest: Request {
+    var method: HTTPMethod = .get
+    var params: RequestParam
+    var path: String { return EndPoint.coinList + "pricemultifull"}
+    
+    init(param: RequestParam) {
+        self.params = param
+    }
+}
+
+let request = CoinListRequest(param: .url(["fsym":"BTC"]))
---
+++ CoinFlowMVVMAgain/Network/EndPoint.swift
@@ -0,0 +1,15 @@
+//
+//  EndPoint.swift
+//  CoinFlowMVVMAgain
+//
+//  Created by APPLE on 2021/01/27.
+//
+
+import Foundation
+
+// 3개의 네트워크 요청 URL
+enum EndPoint {
+    static let coinList: String = "https://min-api.cryptocompare.com/data/"
+    static let coinChartData: String = "https://min-api.cryptocompare.com/data/"
+    static let newsList: String = "http://coinbelly.com/api/get_rss"
+}
